0cf02adcd3c36a91e5f69b09ee1265f245e63d2d
aaronlisman <lismanBusiness@gmail.com>
2018-07-24 12:24:17 -0400

    Add more whitespace between boxes in expression chart (Expression tab)

--- src/pages/resultsView/expression/ExpressionWrapper.tsx
+++ src/pages/resultsView/expression/ExpressionWrapper.tsx
@@ -120,7 +120,7 @@ export default class ExpressionWrapper extends React.Component<ExpressionWrapper
     }
 
     @computed get chartWidth() {
-        return this.sortedLabels.length * (this.widthThreshold ? 25 : 110) + 200;
+        return this.sortedLabels.length * (this.widthThreshold ? 30 : 110) + 200;
     }
 
     @computed get widthThreshold(){
